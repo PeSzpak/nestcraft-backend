fe3c15f9a483348aa46c3e705d3cf141092031be
Pedro Szpak <dropszpalves@gmail.com>
2025-07-15 09:43:08 -0300

    feat: implement DTOs (Data Transfer Objects) - added structure and examples following NestJS best practices

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, ParseIntPipe, Post, Put, Res } from '@nestjs/common';
 import { CoursesService } from './courses.service';
+import { CreateCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course.dto';
 
 
 
@@ -9,22 +11,22 @@ export class CoursesController {
     constructor(private readonly courseService: CoursesService) { }
 
     @Get()
-findAll() {
-    return this.courseService.findAll();
-}
+    findAll() {
+        return this.courseService.findAll();
+    }
     @Get(':id')
     findOne(@Param('id', ParseIntPipe) id: number) {
         return this.courseService.findOne(id);
     }
 
     @Post()
-create(@Body() body) {
-    return this.courseService.create(body);
-}
+    create(@Body() createCourseDTO: CreateCourseDTO) {
+        return this.courseService.create(createCourseDTO);
+    }
 
     @Put(':id')
-    update(@Param('id', ParseIntPipe) id: number, @Body() body) {
-        return this.courseService.update(id, body);
+    update(@Param('id', ParseIntPipe) id: number, @Body() updateCourseDTO: UpdateCourseDTO) {
+        return this.courseService.update(id, updateCourseDTO);
     }
 
     @Delete(':id')
