de44ff351af055472046771fddb06ef12039d360
Pedro Szpak <dropszpalves@gmail.com>
2025-07-16 15:06:32 -0300

    chore: add courseId foreign key to courses_tags table via migration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,6 +3,7 @@ import { dataSourceOptions } from './database.module';
 import { CreateCourseTable1752676142776 } from 'src/migrations/1752676142776-CreateCourseTable';
 import { CreateTagTable1752684319861 } from 'src/migrations/1752684319861-CreateTagTable';
 import { CreateCoursesTagsTable1752686922179 } from 'src/migrations/1752686922179-CreateCoursesTagsTable';
+import { AddCourseIdToCoursesTagsTable1752688220082 } from 'src/migrations/1752688220082-AddCourseIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -11,5 +12,6 @@ export const dataSource = new DataSource({
     CreateCourseTable1752676142776,
     CreateTagTable1752684319861,
     CreateCoursesTagsTable1752686922179,
+    AddCourseIdToCoursesTagsTable1752688220082,
   ],
 });
