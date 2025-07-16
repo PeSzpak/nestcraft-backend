9b4d5a9898da7fa748c2cce56859d84a8607ee87
Pedro Szpak <dropszpalves@gmail.com>
2025-07-16 14:43:34 -0300

    refactor: update CoursesService and controller + create courses_tags table via migration; includes correction for previous duplicate commit

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,14 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCourseTable1752676142776 } from 'src/migrations/1752676142776-CreateCourseTable';
 import { CreateTagTable1752684319861 } from 'src/migrations/1752684319861-CreateTagTable';
+import { CreateCoursesTagsTable1752686922179 } from 'src/migrations/1752686922179-CreateCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCourseTable1752676142776, CreateTagTable1752684319861],
+  migrations: [
+    CreateCourseTable1752676142776,
+    CreateTagTable1752684319861,
+    CreateCoursesTagsTable1752686922179,
+  ],
 });
