3412df22cb47c01667f5010f901b59b1dd839ead
Pedro Szpak <dropszpalves@gmail.com>
2025-07-16 13:58:12 -0300

    chore: create tags table via TypeORM migration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,10 +1,11 @@
 import { DataSource } from "typeorm";
 import { dataSourceOptions } from "./database.module";
 import { CreateCourseTable1752676142776 } from "src/migrations/1752676142776-CreateCourseTable";
+import { CreateTagTable1752684319861 } from "src/migrations/1752684319861-CreateTagTable";
 
 
 export const dataSource = new DataSource({
     ...dataSourceOptions,
     synchronize: false,
-    migrations: [CreateCourseTable1752676142776]
+    migrations: [CreateCourseTable1752676142776, CreateTagTable1752684319861]
 })
