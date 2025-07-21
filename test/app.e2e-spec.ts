31f6ac28cb4dcba24315dacce5df330d856448f5
Pedro Szpak <dropszpalves@gmail.com>
2025-07-21 14:25:00 -0300

    test: setup e2e testing environment for courses module

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,24 +2,59 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from '../src/app.module';
+import 'dotenv/config';
+import { Course } from 'src/courses/entities/courses.entity';
+import { Tag } from 'src/courses/entities/tags.entity';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { CoursesModule } from 'src/courses/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
+  let module: TestingModule;
+  let data: any;
+  let courses: Course[];
+
+  const dataSourceOptions: DataSourceOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: 5433,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: [Course, Tag],
+    synchronize: true,
+  };
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [
+        CoursesModule,
+        TypeOrmModule.forRootAsync({
+          useFactory: async () => {
+            return dataSourceOptions;
+          },
+        }),
+      ],
+    }).compile();
+    app = module.createNestApplication();
+    await app.init();
+
+    data = {
+      name: 'NodeJs',
+      description: 'Node.Js',
+      tags: ['NoseJs', 'NestJs'],
+    };
+  });
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    const dataSource = await new DataSource(dataSourceOptions).initialize();
+    const repository = dataSource.getRepository(Course);
+    courses = await repository.find();
+    await dataSource.destroy();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
   });
 });
